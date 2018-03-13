8fd0a75538ae55f8798914a2a351a3c5e0c4e8e0
Ethan Elliott <ethan.elliott@hotmail.ca>
2018-03-13 17:02:52 -0400

    adding game

--- routes/index.js
+++ routes/index.js
@@ -8,4 +8,16 @@ router.get('/', function(req, res, next) {
 	});
 });
 
+router.get('/horse-race', function(req, res, next) {
+	res.render('horse-race', {
+		title: 'Horse Race'
+	});
+});
+
+router.get('/horse-race-user', function(req, res, next) {
+	res.render('horse-race-user', {
+		title: 'Horse Race'
+	});
+});
+
 module.exports = router;
